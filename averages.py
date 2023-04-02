21ce9b523fd51a85aa1eb679f2526a508ef0619f
CaioP <caiopss99@gmail.com>
2023-04-01 22:38:48 -0300

    Finished program

---
+++ averages.py
@@ -0,0 +1,14 @@
+a=0
+inp = float(input('Digite a primeira nota:'))
+a += inp
+inp = float(input ('Digite a segunda nota:'))
+a += inp     
+inp = float(input('Digite a terceira nota:'))
+a += inp
+inp = float(input('Digite a quarta nota:'))
+a += inp
+
+media  = a /4
+y = format(media)
+
+print("A média aritmética é " + y)
